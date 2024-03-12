988542b75b0e84eea6516b784c02498c6681713a
Carson Spaniel <carsonspaniel@gmail.com>
2024-03-12 01:08:52 -0500

    adding new graphics and pages

--- horsepower.py
+++ horsepower.py
@@ -4,11 +4,17 @@ import time
 import random
 import math
 
+DEV = True
+
+# # Print out the commands
+# for command_name in obd.commands.__dict__.values():
+#     print(command_name)
+
 # Initialize Pygame
 pygame.init()
 
 # Screen dimensions for a landscape 3.5-inch display
-SCREEN_WIDTH, SCREEN_HEIGHT = 480, 320
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 480
 FPS = 30
 
 # Colors
@@ -20,50 +26,63 @@ GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-RPM_MAX = 8000
-SHIFT = 6500
-
 # Fonts
-font_large = pygame.font.Font(None, 120)
-font_medium = pygame.font.Font(None, 48)
-font_small = pygame.font.Font(None, 36)
+font_large = pygame.font.Font("./digital-7.ttf", 120)
+font_medium = pygame.font.Font("./digital-7.ttf", 48)
+font_small = pygame.font.Font("./digital-7.ttf", 36)
 
 connect = False
 
-for i in range(3):
-    print('Attempting to connect...')
+if not DEV:
+    for i in range(3):
+        print('\nAttempting to connect...')
 
-    # Connect to the OBD-II adapter
-    connection = obd.OBD(portstr='COM5')
+        # Connect to the OBD-II adapter
+        connection = obd.OBD(portstr='COM5')
 
-    # Print a message indicating connection
-    if connection.is_connected():
-        print("Connected to OBD-II adapter. Ready to log data.")
-        connect = True
-        break
+        # Print a message indicating connection
+        if connection.is_connected():
+            print("Connected to OBD-II adapter. Ready to log data.")
+            connect = True
+            break
+        else:
+            print("Could not connect to OBD-II adapter.")
+
+    if not connect:
+        print('Failed. Exiting...')
+        exit()
+
+# Function to calculate MPG
+def calculate_mpg(speed, maf):
+    if speed == 0:
+        return 0
+    gph = (maf / 14.7) * 0.746  # grams per hour to gallons per hour
+    if DEV:
+        mpg = speed / gph
     else:
-        print("Could not connect to OBD-II adapter. Exiting...")
-
-if not connect:
-    exit()
+        speed.to('mile/hour') / gph
+    return round(mpg,1)
 
 # Function to save max horsepower data to a file
-def save_max_horsepower(max_horsepower, max_horsepower_rpm):
-    with open("max_horsepower.txt", "w") as file:
-        file.write(f"{max_horsepower},{max_horsepower_rpm}")
+def save_rpm(RPM_MAX, SHIFT):
+    with open("RPM.txt", "w") as file:
+        file.write(f"{RPM_MAX},{SHIFT}")
 
 # Function to load max horsepower data from a file
-def load_max_horsepower():
+def load_rpm():
     try:
-        with open("max_horsepower.txt", "r") as file:
+        with open("RPM.txt", "r") as file:
             data = file.read().split(",")
-            max_horsepower = float(data[0])
-            max_horsepower_rpm = int(data[1])
-    except Exception:
-        max_horsepower = 0
-        max_horsepower_rpm = 0
+            max = int(data[0])
+            shift = int(data[1])
+    except Exception as e:
+        print(e)
+        max = 8000
+        shift = 6500
 
-    return max_horsepower, max_horsepower_rpm
+    return max, shift
+
+RPM_MAX,SHIFT = load_rpm()
 
 # Function to calculate horsepower
 def calculate_horsepower(torque, rpm):
@@ -99,6 +118,8 @@ def display_logo(screen):
         rotated_rect = rotated_logo.get_rect(center=logo_rect.center)
         screen.blit(rotated_logo, rotated_rect)
 
+        draw_text(screen, "Developed by Carson Spaniel", font_small, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT - 30)
+
         pygame.display.flip()
 
         scale += 0.004
@@ -107,11 +128,13 @@ def display_logo(screen):
 
 # Main function for the Pygame interface
 def main():
+    global RPM_MAX
+    global SHIFT
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Horsepower and RPM Display")
     clock = pygame.time.Clock()
 
-    pages = ["RPM", "Horsepower", "Both", "Off"]
+    pages = ["RPM", "MPG", "Both"] #, "Off"
     current_page = 0
 
     # Load the last visited page
@@ -126,48 +149,102 @@ def main():
     logging = True
 
     # Display Chevrolet logo for 5 seconds
-    # display_logo(screen)
 
-    # Load the initial maximum horsepower and RPM
-    max_horsepower, max_horsepower_rpm = load_max_horsepower()
+    display_logo(screen)
 
-    rpm = 650
+    if DEV:
+        rpm = 650
+        fuel_level = random.randint(0,100)
+        speed = 10
+        maf = 6
     while logging:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 logging = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # Left mouse button
-                    # Check if the click was on the right half of the screen
-                    if event.pos[0] > SCREEN_WIDTH / 2:
-                        current_page = (current_page + 1) % len(pages)
-                    else:  # Click on the left half of the screen
+                    # Check top left corner for page change
+                    if event.pos[0] < SCREEN_WIDTH // 10 and event.pos[1] < SCREEN_HEIGHT // 10:
                         current_page = (current_page - 1) % len(pages)
+                    # Check top right corner for page change
+                    elif event.pos[0] > SCREEN_WIDTH - SCREEN_WIDTH // 10 and event.pos[1] < SCREEN_HEIGHT // 10:
+                        current_page = (current_page + 1) % len(pages)
+                    else:
+                        if pages[current_page] == "RPM":
+                            # Check for collision with increase rectangle
+                            if event.pos[0] < SCREEN_WIDTH * 0.2+25+SCREEN_WIDTH*.1 and event.pos[0] > SCREEN_WIDTH * 0.2+25 and event.pos[1] < SCREEN_HEIGHT*.3+SCREEN_HEIGHT*.1 and event.pos[1] > SCREEN_HEIGHT*.3:
+                                RPM_MAX += 100  # Increase RPM_MAX by 100
 
-        # Query for RPM and Torque
-        cmd_rpm = obd.commands.RPM
-        cmd_torque = obd.commands.ENGINE_LOAD  # Torque can be estimated from ENGINE_LOAD
-        response_rpm = connection.query(cmd_rpm)
-        response_torque = connection.query(cmd_torque)
+                                if RPM_MAX > 50000:
+                                    RPM_MAX = 50000
 
-        if not response_rpm.is_null() and not response_torque.is_null():
-            rpm = response_rpm.value.magnitude
-            torque = response_torque.value.magnitude
+                                # Save the new max horsepower data
+                                save_rpm(RPM_MAX,SHIFT)
 
-        # rpm = random.randint(max(0,rpm-200), min(rpm+270,RPM_MAX))
-        # torque = random.randint(200, 250)
+                            # Check for collision with decrease rectangle
+                            elif event.pos[0] < SCREEN_WIDTH * 0.2+25+SCREEN_WIDTH*.1 and event.pos[0] > SCREEN_WIDTH * 0.2+25 and event.pos[1] < SCREEN_HEIGHT-SCREEN_HEIGHT*.3+SCREEN_HEIGHT*.1 and event.pos[1] > SCREEN_HEIGHT-SCREEN_HEIGHT*.3:
+                                RPM_MAX -= 100  # Decrease RPM_MAX by 100
+                                if RPM_MAX == 0:
+                                    RPM_MAX = 100
 
-        horsepower = calculate_horsepower(torque, rpm)
+                                if SHIFT > RPM_MAX:
+                                    SHIFT = RPM_MAX
 
-        # Update maximum horsepower and RPM if current is greater
-        if horsepower > max_horsepower:
-            max_horsepower = horsepower
-            max_horsepower_rpm = rpm
-            # Save the new max horsepower data
-            save_max_horsepower(max_horsepower, max_horsepower_rpm)
+                                # Save the new max horsepower data
+                                save_rpm(RPM_MAX,SHIFT)
+
+                            # Check for collision with increase rectangle
+                            elif event.pos[0] < SCREEN_WIDTH * 0.7-25+SCREEN_WIDTH*.1 and event.pos[0] > SCREEN_WIDTH * 0.7-25 and event.pos[1] < SCREEN_HEIGHT*.3+SCREEN_HEIGHT*.1 and event.pos[1] > SCREEN_HEIGHT*.3:
+                                SHIFT += 100  # Increase SHIFT by 100
+
+                                if SHIFT > RPM_MAX:
+                                    SHIFT = RPM_MAX
+
+                                # Save the new max horsepower data
+                                save_rpm(RPM_MAX,SHIFT)
+
+                            # Check for collision with decrease rectangle
+                            elif event.pos[0] < SCREEN_WIDTH * 0.7-25+SCREEN_WIDTH*.1 and event.pos[0] > SCREEN_WIDTH * 0.7-25 and event.pos[1] < SCREEN_HEIGHT-SCREEN_HEIGHT*.3+SCREEN_HEIGHT*.1 and event.pos[1] > SCREEN_HEIGHT-SCREEN_HEIGHT*.3:
+                                SHIFT -= 100  # Decrease SHIFT by 100
+
+                                if SHIFT == 0:
+                                    SHIFT = 100
+
+                                # Save the new max horsepower data
+                                save_rpm(RPM_MAX,SHIFT)
+
+        if DEV:
+            rpm = random.randint(max(0,rpm-200), min(rpm+270,RPM_MAX))
+            speed = random.randint(max(0,speed-5), min(speed+5,80))
+            maf = random.randint(max(1,maf-5), min(maf+5,80))
+            mpg = calculate_mpg(speed, maf)
+            if fuel_level<=0:
+                fuel_level=100
+            fuel_level -= .1
+        else:
+            # Query for RPM and Torque
+            response_rpm = connection.query(obd.commands.RPM)
+            response_fuel_level = connection.query(obd.commands.FUEL_LEVEL)
+            speed_response = connection.query(obd.commands.SPEED)  # Vehicle speed
+            maf_response = connection.query(obd.commands.MAF)      # Mass Air Flow
+
+            if speed_response.success and maf_response.success:
+                speed = speed_response.value.to('mile/hour')
+                maf = maf_response.value.to('gram/second')
+                mpg = calculate_mpg(speed.magnitude, maf.magnitude)
+
+            if not response_rpm.is_null():
+                rpm = round(response_rpm.value.magnitude,0)
+
+            if not response_fuel_level.is_null():
+                fuel_level = response_fuel_level.value.magnitude
 
         # Clear the screen
         screen.fill(BLACK)
+
+        # Draw page buttons
+        draw_text(screen, "<", font_medium, WHITE, SCREEN_WIDTH*.05, SCREEN_HEIGHT * .05)
+        draw_text(screen, ">", font_medium, WHITE, SCREEN_WIDTH -SCREEN_WIDTH*.05, SCREEN_HEIGHT * .05)
 
         # Draw page indicators (circles)
         circle_radius = 5
@@ -198,6 +275,26 @@ def main():
         pygame.draw.line(screen, WHITE, (SCREEN_WIDTH * 0.8, SCREEN_HEIGHT*.2), (SCREEN_WIDTH, SCREEN_HEIGHT*.2), 2)
         pygame.draw.line(screen, WHITE, (SCREEN_WIDTH * 0.8, SCREEN_HEIGHT*.2), (SCREEN_WIDTH * 0.8, SCREEN_HEIGHT), 2)
 
+        # Calculate the height of the filled portion based on percentage
+        fuel_height = math.floor((SCREEN_HEIGHT*.7) * fuel_level/100)
+
+        # Draw the filled portion
+        if fuel_level > 75:
+            fuel_color = GREEN
+        elif fuel_level <=75 and fuel_level > 50:
+            fuel_color = YELLOW
+        elif fuel_level <=50 and fuel_level > 30:
+            fuel_color = ORANGE
+        else:
+            fuel_color = RED
+        
+        draw_text(screen, f"{round(fuel_level,1)}%", font_medium, WHITE, SCREEN_WIDTH*.1, SCREEN_HEIGHT*.25)
+        pygame.draw.rect(screen, fuel_color, (0, SCREEN_HEIGHT - fuel_height, SCREEN_WIDTH * 0.2, fuel_height))
+
+        pygame.draw.line(screen, WHITE, (0, SCREEN_HEIGHT*.3), (SCREEN_WIDTH*.2, SCREEN_HEIGHT*.3), 2)
+        pygame.draw.line(screen, WHITE, (SCREEN_WIDTH * 0.2, SCREEN_HEIGHT*.3), (SCREEN_WIDTH * 0.2, SCREEN_HEIGHT), 2)
+
+
         for i, page in enumerate(pages):
             if page != 'Off':
                 color = WHITE if i == current_page else BLACK
@@ -215,19 +312,13 @@ def main():
         # Calculate starting position to center horizontally
         start_x = (SCREEN_WIDTH - total_circle_width) // 2
         circle_x = start_x + circle_radius + circle_spacing
-        circle_y = circle_radius + circle_spacing #+ 20
+        circle_y = circle_radius + circle_spacing
 
         # Colors for each light
         light_colors = [YELLOW, YELLOW, ORANGE, ORANGE, RED, RED, PURPLE, PURPLE]
 
         for i in range(8):
             color = light_colors[i]
-
-            # Adjust y-coordinate for arch pattern
-            # if i < 3:
-            #     circle_y -= 5
-            # elif i > 5:
-            #     circle_y += 5
 
             pygame.draw.circle(screen, WHITE, (circle_x, circle_y), circle_radius + 4)
             pygame.draw.circle(screen, BLACK, (circle_x, circle_y), circle_radius + 2)
@@ -241,21 +332,35 @@ def main():
             # Draw RPM section
             draw_text(screen, "RPM", font_medium, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4 + 20)
             draw_text(screen, str(rpm), font_large, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 +20)
+            draw_text(screen, "Max", font_small, WHITE, SCREEN_WIDTH*.28, SCREEN_HEIGHT // 2)
+            draw_text(screen, str(RPM_MAX), font_medium, WHITE, SCREEN_WIDTH*.28, SCREEN_HEIGHT // 2 +40)
+            draw_text(screen, "Shift", font_small, WHITE, SCREEN_WIDTH*.72, SCREEN_HEIGHT // 2)
+            draw_text(screen, str(SHIFT), font_medium, WHITE, SCREEN_WIDTH*.72, SCREEN_HEIGHT // 2 +40)
 
-        elif pages[current_page] == "Horsepower":
+            pygame.draw.rect(screen, GREEN, (SCREEN_WIDTH * 0.2+25, SCREEN_HEIGHT*.3, SCREEN_WIDTH * 0.1, SCREEN_HEIGHT*.1))
+            pygame.draw.rect(screen, RED, (SCREEN_WIDTH * 0.2+25, SCREEN_HEIGHT-SCREEN_HEIGHT*.3, SCREEN_WIDTH * 0.1, SCREEN_HEIGHT*.1))
+
+            draw_text(screen, "+", font_medium, BLACK, SCREEN_WIDTH * 0.2+25+SCREEN_WIDTH*.05, SCREEN_HEIGHT*.3+SCREEN_HEIGHT*.05)
+            draw_text(screen, "-", font_medium, BLACK, SCREEN_WIDTH * 0.2+25+SCREEN_WIDTH*.05, SCREEN_HEIGHT-SCREEN_HEIGHT*.3+SCREEN_HEIGHT*.05)
+
+            pygame.draw.rect(screen, GREEN, (SCREEN_WIDTH * 0.7-25, SCREEN_HEIGHT*.3, SCREEN_WIDTH * 0.1, SCREEN_HEIGHT*.1))
+            pygame.draw.rect(screen, RED, (SCREEN_WIDTH * 0.7-25, SCREEN_HEIGHT-SCREEN_HEIGHT*.3, SCREEN_WIDTH * 0.1, SCREEN_HEIGHT*.1))
+
+            draw_text(screen, "+", font_medium, BLACK, SCREEN_WIDTH * 0.7-25+SCREEN_WIDTH*.05, SCREEN_HEIGHT*.3+SCREEN_HEIGHT*.05)
+            draw_text(screen, "-", font_medium, BLACK, SCREEN_WIDTH * 0.7-25+SCREEN_WIDTH*.05, SCREEN_HEIGHT-SCREEN_HEIGHT*.3+SCREEN_HEIGHT*.05)
+
+
+        elif pages[current_page] == "MPG":
             # Draw Horsepower section
-            draw_text(screen, "Horsepower", font_medium, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4 + 20)
-            draw_text(screen, str(round(horsepower, 2)), font_large, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-
-            # Display Max HP and corresponding RPM
-            draw_text(screen, f"{str(round(max_horsepower, 2))} @ {max_horsepower_rpm} RPM", font_small, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 80)
+            draw_text(screen, "Instant MPG", font_medium, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4 + 20)
+            draw_text(screen, str(round(mpg, 2)), font_large, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2+20)
 
         elif pages[current_page] == "Both":
             # Draw RPM and Horsepower on separate lines
             draw_text(screen, "RPM", font_medium, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4)
-            draw_text(screen, "Horsepower", font_medium, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 40)
-            draw_text(screen, f"{rpm}", font_large, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4 + 50)
-            draw_text(screen, f"{round(horsepower, 2)}", font_large, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 90)
+            draw_text(screen, "Instant MPG", font_medium, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 40)
+            draw_text(screen, f"{rpm}", font_large, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4 + 60)
+            draw_text(screen, str(round(mpg, 2)), font_large, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 100)
 
         elif pages[current_page] == "Off":
             screen.fill(BLACK)
@@ -267,7 +372,7 @@ def main():
         pygame.display.flip()
         clock.tick(FPS)
 
-        time.sleep(.05)  # Wait for 0.1 second before next iteration
+        time.sleep(.05)  # Wait for 0.05 second before next iteration
 
     print("Logging stopped.")
 
