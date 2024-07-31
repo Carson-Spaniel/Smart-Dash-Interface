d9f09c47b25c54a485e29d5c04699bef7ae5ba6e
Carson_Spaniel <carsonspaniel@gmail.com>
2024-07-31 16:43:40 -0500

    forgot to change this

--- Experimental/server.py
+++ Experimental/server.py
@@ -88,7 +88,7 @@ def shutdown():
 # Run the Flask application
 if __name__ == '__main__':
     print("Starting Flask application")
-    file_path = "Experimental/Maps/georgetown_map.graphml"
+    file_path = "Experimental/Maps/map.graphml"
 
     print(f"Loading street network from file: {file_path}")
 
