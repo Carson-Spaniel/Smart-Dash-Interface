827e539ddc0e0d0cfed0535416b26612bbd997ef
Carson_Spaniel <carsonspaniel@gmail.com>
2024-07-31 16:43:12 -0500

    more changes to downloading

--- Experimental/download_map.py
+++ Experimental/download_map.py
@@ -6,14 +6,21 @@ dir_path = 'Maps'
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
 
-def download_and_save_as_graphml(file_path="Maps/georgetown_map.graphml"):
-    # Define the bounding box coordinates for Georgetown, Texas (left, bottom, right, top)
-    left, bottom, right, top = -97.84662785075604, 30.601071447149565, -97.60809443188543, 30.748176850389648
+def download_and_save_as_graphml(file_path="Maps/map.graphml"):
+    # Define the bounding box coordinates
+    bottom, left = 30.610327077517546, -97.82914630749687
+    top, right = 33.34599146415902, -96.27847196976995
 
     # Create a graph from the bounding box
-    G = ox.graph_from_bbox(north=top, south=bottom, east=right, west=left, network_type='drive')
+    G = ox.graph_from_bbox(bbox=(top,bottom,right,left), network_type='drive')
     
-    # Save the graph to a GraphML file
-    ox.save_graphml(G, file_path)
+    # Filter out edges that do not have a speed limit
+    edges_with_speed = [(u, v, k) for u, v, k, data in G.edges(keys=True, data=True) if 'maxspeed' in data]
+
+    # Create a new graph with only edges that have speed limits
+    H = G.edge_subgraph(edges_with_speed).copy()
+
+    # Save the filtered graph to a GraphML file
+    ox.save_graphml(H, file_path)
 
 download_and_save_as_graphml()
