37d99c790d47dc677d2e4eee5b0d614728a30a03
Carson_Spaniel <carsonspaniel@gmail.com>
2024-09-01 21:52:13 -0500

    small typo

--- Dash/install.sh
+++ Dash/install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing..."
 
-sudo mv boot.desktop ~/.config/autostart -y # Make it run at start
+sudo mv boot.desktop ~/.config/autostart # Make it run at start
 python3 -m venv env # Create virtual environment
 source env/bin/activate # Activate the virtual environment
 pip install -r requirements.txt # Install requirements
