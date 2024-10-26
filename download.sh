eb1d38ad68bc091db9728d53f1eb9acf090e4b50
Carson Spaniel <88287064+Carson-Spaniel@users.noreply.github.com>
2024-10-25 23:03:25 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run these commands to download and install the latest version
+# Run these commands when connected to wifi to download and install the latest version
 cd /home/pi/
 wget -O Dash.tar.xz https://github.com/Carson-Spaniel/Smart-Dash-Interface/releases/latest/download/Dash.tar.xz
 tar -xJvf Dash.tar.xz
